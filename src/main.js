f4960b5bfedfd5ab358b1e9e914044e501fa4e4d
TCS <caffeinesage44@gmail.com>
2024-01-26 13:46:49 +0000

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import HomePage from './pages/HomePage.vue'
+import AboutPage from './pages/AboutPage.vue'
+import ArticlePage from './pages/ArticlePage.vue'
+
+const routes = [
+    {
+        path: "/",
+        component: HomePage,
+        name: 'home'
+    },
+    {
+        path: "/about",
+        component: AboutPage,
+        name: 'about'
+    },
+    {
+        path: "/articles/:id",
+        component: ArticlePage,
+        name: 'articles'
+    }
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+});
+
+const app = createApp(App); 
+app.use(router);
+app.mount('#app')
---
+++ src/pages/ArticlePage.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Welcome to the Article Pages!</h1>
+        <p>The id parameter vue is: {{  $route.params.id }}</p>
+    </div>
+</template>
